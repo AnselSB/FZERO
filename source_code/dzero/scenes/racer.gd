7862734b6bafca44a5af9a1f490aac42b9633e73
SandersWifeBernie <117328851+AnselSB@users.noreply.github.com>
2025-01-26 17:16:53 -0700

    implement turning mechanic
    remember to add rotation of the model so it coincides with turning

--- source_code/dzero/scenes/racer.gd
+++ source_code/dzero/scenes/racer.gd
@@ -1,14 +1,17 @@
 extends CharacterBody3D
 
 
-const SPEED = 100.0
+const SPEED = 500.0
 const JUMP_VELOCITY = 4.5
 const ACCELERATION = 0.5
 const THETA =  1
+const UP = Vector3(0, 1, 0)
 # going to store a vector that will represent the direction of the racer, this direction will change from the directional inputs
 # by default goes in direction into the screen (away from the face of the user) 
 var direc = Vector3(0, 0, -1)
 var initialPos = Vector3(position)
+var rightTurn = Vector3(1, 0, 0)
+var leftTurn = Vector3(-1, 0, 0)
 # will handle all of the movement based controls for the racer, here we need to first get it to move forward, and for now we won't add gravity
 func _physics_process(delta):
 	# Add the gravity, may need not sure at this point	
@@ -18,22 +21,21 @@ func _physics_process(delta):
 	if Input.is_action_pressed("gas"):
 			# check for direction changes 
 		if Input.is_action_pressed("left"):
-		# when going left we want to have the object slowly turn to the left, this will involve rotating the direction clockwise about the y axis
-		# also need to normalize the vector 
-			direc = direc.rotated(Vector3(0, 1, 0),THETA * delta)
-			self.rotate_y(THETA * delta)
+		#	get the left turn (recall right hand rule)
+			leftTurn = UP.cross(direc)
+			# add left turn weight to the direction than normalize
+			direc = direc + Vector3(leftTurn.x * 0.01, leftTurn.y * 0.01, leftTurn.z * 0.01)
 			direc.normalized()
-	# repeat for the other direction, elif because we don't want to be able to go left and right simultaneously 
 		elif Input.is_action_pressed("right"):
-			direc = direc.rotated(Vector3(0, 1, 0), -1 * THETA * delta)
-			self.rotate_y(THETA * delta)
+			# generate the right turn using the cross product (follow right hand rule), make sure to normalize
+			rightTurn = direc.cross(UP)
+			# add to the direction the right turn direction and normalize
+			direc = direc + Vector3(rightTurn.x * 0.01, rightTurn.y * 0.01, rightTurn.z * 0.01)
 			direc.normalized()
-		#add to the position the speed in the direction of the racer multiplied by the delta
-		# we'll want to create a "limit" to the velocity, if the magnitude is a certain speed it no longer gets faster
-		if velocity.length() < 25:
-			velocity.z += SPEED * delta * direc.z * ACCELERATION 
-			velocity.y += SPEED * delta * direc.y  * ACCELERATION
-			velocity.x += SPEED * delta * direc.x  * ACCELERATION
+		# set velocity
+		velocity.z = SPEED * delta * direc.z
+		velocity.y = SPEED * delta * direc.y
+		velocity.x = SPEED * delta * direc.x
 	else: 
 		velocity = Vector3(0, 0, 0)
 	move_and_slide()
